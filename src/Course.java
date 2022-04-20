c0622b3b7f1fb478231f1ad452b38fbec5a4eb8c
Sand630 <taskitup@live.com>
2022-04-20 21:07:13 +0300

    Something

--- src/Course.java
+++ src/Course.java
@@ -1,7 +1,59 @@
 public class Course {
+    private String courseNum;
+    private String courseName;
     private int credits;
+    private int section;
 
-    public int getCredits() {
-        return credits;
+    public Course()
+    {
+        courseNum = "";
+        courseName = "";
+        credits = 0;
+        section = 0;
     }
+
+    public Course(String Num,String Name,int cred, int sec)
+    {
+        courseNum = Num;
+        courseName = Name;
+        credits = cred;
+        section = sec;
+    }
+
+    public void setCourseNum(String Num)
+    {courseNum = Num;}
+    public String getCourseNum()
+    {return courseNum;}
+
+    public void setCourseName(String Name)
+    {courseName = Name ;}
+    public String getCourseName()
+    {return courseName;}
+
+    public void setCredits(int cred)
+    {credits = cred;}
+    public int getCredits()
+    {return credits;}
+
+    public void setSection(int sec)
+    {section = sec;}
+    public int getSection()
+    {return section;}
+
+   /* public boolean equals(Course set)
+    {
+            if (set.getCourseNum()==courseNum && set.getCourseName()== courseName
+        && set.getCredits()==credits && set.getSection()==section) return true;
+            else return false;
+    } */
+
+    public String toString()
+    {
+        String results = "Course Number: " + courseNum
+                + "\nCourse Name: " + courseName
+                + "\nCredits: " + credits
+                + "\nSection: " + section ;
+        return results ;
+    }
+
 }
--- src/Student.java
+++ src/Student.java
@@ -9,24 +9,66 @@ public class Student {
     private String email;
     private ArrayList<Course> coursesRegistered;
 
-    public void printCoursesRegistered(){}
+    public Student()
+    {
+        idNum = 0 ;
+        firstName = "";
+        lastName = "";
+        gender = 'X';
+        email = "";
+        coursesRegistered = new ArrayList<Course>(6);
+    }
+    public Student(long idNum,String firstName,String lastName,char gender,String email)
+    {
+        this.idNum = idNum ;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.email = email;
+        coursesRegistered = new ArrayList<Course>(6);
+    }
 
+
+    public void setfirstName(String fName)
+    {firstName = fName;}
     public String getFirstName() {
         return firstName;
     }
 
+    public void setLastName(String lName)
+    {lastName =lName;}
     public String getLastName() {
         return lastName;
     }
 
+    public void setGender(char Gen)
+    {gender = Gen;}
     public char getGender() {
         return gender;
     }
 
+    public void setEmail(String elecMail)
+    {email = elecMail;}
     public String getEmail() {
         return email;
     }
 
+    public void setIdNum (long IdNo)
+    {idNum = IdNo;}
     public long getIdNum(){ return idNum; }
-    public ArrayList<Course> getCoursesRegistered(){ return coursesRegistered; }
+
+   /* public boolean equals(Student set)
+    {
+        if(idNum==set.getIdNum())  return true;
+    }*/
+
+    public ArrayList<Course> getCoursesRegistered()
+    { return coursesRegistered; }
+    public void printCoursesRegistered()
+    {
+
+    }
+
+
+
 }
